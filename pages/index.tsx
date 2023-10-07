1aa1d1378d1e1abb10a78c0b4a83f1be59b58ed1
cherr0406 <82948891+cherr0406@users.noreply.github.com>
2023-10-07 14:42:59 +0900

    feat: add descriptions to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,6 +79,8 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>材料リストを取ってくるレシピのURLを入力してください</p>
+          <p>対応サイト: bazurecipe.com, cookien.com</p>
+          <p>※取得ボタンをむやみに押さないでください。</p>
           <div className={styles.form}>
             <input type="text" id="url" className={styles.input} placeholder="https://example.com" />
             <button className={styles.button} onClick={clicked}>
